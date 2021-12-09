104cb357207f433e6fe5393ab67535881ac39747
thephotogenicbug <navinkumarachari@gmail.com>
2021-12-09 15:04:22 +0530

    material-ui5 - grid system
    Signed-off-by: thephotogenicbug <navinkumarachari@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import GridMUI from './playground/GridMUI.js';
 function App() {
   return (
-    <div>
-        <Navbar />
-        <Header />
-    </div>
+    <>
+    <GridMUI/>
+        {/* <Navbar />
+        <Header /> */}
+    </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { Box } from '@mui/system'
-import { Typography } from '@mui/material'
+import React from "react";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 const Header = () => {
-    return (
-        <Box sx={{minHeight:'85vh', background:'url(images/header.png)',backgroundPosition:'center', backgroundSize:'cover' }}>
-            <Typography variant="h1">Header</Typography>
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        minHeight: "86vh",
+        background: "url(images/header.png)",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Box
+        component="div"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          height: "80vh",
+          ml:3,
+        }}
+      >
+        <Typography
+          sx={{ fontFamily: "cursive", fontWeight: "bold", color: "#f8bbd0" }}
+          variant="h2"
+        >
+          Find Your New Favorite
+        </Typography>
+        <Typography
+          sx={{ fontFamily: "cursive", fontWeight: "bold", color: "#f8bbd0" }}
+          variant="h3"
+        >
+          Collection at Winter
+        </Typography>
+        <Typography
+          sx={{ fontFamily: "cursive", fontWeight: "bold", color: "#f8bbd0" }}
+          variant="h4"
+        >
+          Sales 2021
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
